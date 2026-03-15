56b09070273a716ea39bcc8af8053fcfa7e6731b
livrimfr <contact.livrim@gmail.com>
2026-03-15 19:34:09 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/livrim/',
-  module.exports = {
-  plugins: [
-    'babel-plugin-react-compiler',
+      babel: {
+        plugins: ['babel-plugin-react-compiler'],
 })
